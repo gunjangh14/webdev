e9ddb8563131964db5a50afb4d970dd286508479
William Shea <wjshea@mac.com>
2013-04-08 11:55:07 -0400

    minor updates

--- app/js/utils/tdaapiutil.js
+++ app/js/utils/tdaapiutil.js
@@ -12,3 +12,16 @@ function makeTDARequest(url, postdata, authtoken, session-id){
     var response = {};
     return response;
 }
+
+function getQuoteSnapShot(symbol[]){
+    // This takes a symbol array as an input because you can have more than one input to the api
+    // logic should check the symbol quote cache first before requesting fresh data.  This call may make sense to be made in
+    // in a worker thread if it exists
+
+}
+
+function getSymbolLookup(text){
+ // using market on demand here becuase it is better and does note require login
+}
+
+
